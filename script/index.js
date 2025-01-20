cacd5c18561f05b12134daf37f6106eb98f81941
Tausifqureshi <tausifqureshi504@gmail.com>
2025-01-20 15:27:58 +0530

    Appwigte

--- script/index.js
+++ script/index.js
@@ -8,6 +8,7 @@ async function getData() {
   try {
     const response = await fetch("https://fakestoreapi.com/products");
     const data = await response.json();
+    console.log("data", data);
     renderProducts(data);
   } catch (error) {
     console.error("Error fetching data: ", error);
